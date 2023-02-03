e80af101525f3e03745745a8d74eaeedf10e54f3
Titus Wormer <tituswormer@gmail.com>
2023-02-03 20:30:05 +0100

    Refactor code-style
    *   Add more docs to JSDoc
    *   Add support for `null` in input of API types

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * @typedef {import('mdast-util-from-markdown').Extension} FromMarkdownExtension
  * @typedef {import('mdast-util-to-markdown').Options} ToMarkdownExtension
+ *
  * @typedef {import('mdast-util-mdx-expression').MdxFlowExpression} MdxFlowExpression
  * @typedef {import('mdast-util-mdx-expression').MdxTextExpression} MdxTextExpression
  * @typedef {import('mdast-util-mdxjs-esm').MdxjsEsm} MdxjsEsm
@@ -9,7 +10,6 @@
  * @typedef {import('mdast-util-mdx-jsx').MdxJsxExpressionAttribute} MdxJsxExpressionAttribute
  * @typedef {import('mdast-util-mdx-jsx').MdxJsxFlowElement} MdxJsxFlowElement
  * @typedef {import('mdast-util-mdx-jsx').MdxJsxTextElement} MdxJsxTextElement
- *
  * @typedef {import('mdast-util-mdx-jsx').ToMarkdownOptions} ToMarkdownOptions
  */
 
@@ -21,15 +21,29 @@ import {mdxJsxFromMarkdown, mdxJsxToMarkdown} from 'mdast-util-mdx-jsx'
 import {mdxjsEsmFromMarkdown, mdxjsEsmToMarkdown} from 'mdast-util-mdxjs-esm'
 
 /**
- * @return {Array<FromMarkdownExtension>}
+ * Create an extension for `mdast-util-from-markdown` to enable MDX (ESM, JSX,
+ * expressions).
+ *
+ * @returns {Array<FromMarkdownExtension>}
+ *   Extension for `mdast-util-from-markdown` to enable MDX (ESM, JSX,
+ *   expressions).
+ *
+ *   When using the syntax extensions with `addResult`, ESM and expression
+ *   nodes will have `data.estree` fields set to ESTree `Program` node.
  */
 export function mdxFromMarkdown() {
   return [mdxExpressionFromMarkdown, mdxJsxFromMarkdown(), mdxjsEsmFromMarkdown]
 }
 
 /**
- * @param {ToMarkdownOptions} [options]
- * @return {ToMarkdownExtension}
+ * Create an extension for `mdast-util-to-markdown` to enable MDX (ESM, JSX,
+ * expressions).
+ *
+ * @param {ToMarkdownOptions | null | undefined} [options]
+ *   Configuration.
+ * @returns {ToMarkdownExtension}
+ *   Extension for `mdast-util-to-markdown` to enable MDX (ESM, JSX,
+ *   expressions).
  */
 export function mdxToMarkdown(options) {
   return {
